f83b48934d15455938eda3efad9b5b6b07c4c834
JOY <joy.kemunto@student.moringaschool.com>
2022-06-10 11:27:05 +0300

    commit tree 81040aa050b6e9d230c467fd14ad97a407a29c60

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+//My scope code is written below
+
+const leastFavoriteCustomer = "some initial value"
+//declaring the customers name to be bob
+var customerName = 'bob'
+
+//function name(){}
+function upperCaseCustomerName() {
+//make the customers name to be in uppercase
+  customerName = customerName.toUpperCase()
+  
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+// reassign
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'truoy'
+}
+
